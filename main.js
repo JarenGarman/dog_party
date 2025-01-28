449e6d8265c8fee4b7449f7ae6001d989a9e5348
Jaren Garman <jarengarman@protonmail.com>
2025-01-28 17:19:19 -0500

    refactor: Extract displayDogName from event listener

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const form = document.querySelector('.hero form')
 const input = document.querySelector('#dog-name')
 const headerText = document.querySelector('.hero h1 strong')
-form.addEventListener('submit', function (event) {
+const displayDogName = (event) => {
   event.preventDefault()
   const dogName = input.value
   headerText.innerText = dogName
-})
+}
+form.addEventListener('submit', displayDogName)
